14587e34fa25f0e2b94efd47ae5bbacb34444a06
Shubham Arya <shubhamarya.4596@gmail.com>
2025-04-20 18:26:15 +0530

    adding google and github oauth2 configurations.

---
+++ src/main/java/com/mine/SpringOAuth2/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mine.SpringOAuth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
